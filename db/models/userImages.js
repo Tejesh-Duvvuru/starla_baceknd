9fb1ad394e62e34155299ee39818ab2bbf37158c
Tejesh-Duvvuru <saitejesh1919@gmail.com>
2025-08-12 13:35:46 +0530

    add get router and update data types

--- db/models/userImages.js
+++ db/models/userImages.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize")
-const {db} = require("../config.js");
+const { db } = require("../config.js");
 
-const userImage = db.define("user_image", {
+const userImage = db.define("user_upload_image", {
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -13,14 +13,14 @@ const userImage = db.define("user_image", {
     title: {
         type: Sequelize.STRING
     },
-    description:{
-        type: Sequelize.STRING
+    description: {
+        type: Sequelize.TEXT
     },
     stone_count: {
         type: Sequelize.STRING
     },
     signedUrl: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
     }
 })
 
@@ -31,7 +31,7 @@ userImage.sync().then(() => {
 
 async function addImageContextData(data) {
     try {
-        
+
         const dbResult = await userImage.create(data)
         return { result: true, value: dbResult?.dataValues }
     } catch (error) {
@@ -40,6 +40,24 @@ async function addImageContextData(data) {
     }
 }
 
+async function getImageData() {
+    try {
+        const result = await userImage.findAll()
+        let data = []
+
+        result.forEach(item => {
+            data.push(item?.dataValues)
+        })
+
+        return { result: true, value: data }
+    } catch (error) {
+        console.log("error", error)
+        return { result: false, value: error }
+    }
+}
+
+
 module.exports = {
-    addImageContextData
+    addImageContextData,
+    getImageData
 }
--- routes/userImage.js
+++ routes/userImage.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = new express.Router()
 const multer = require("multer")
-const {uploadToS3} = require("../helpers")
-const {addImageContextData} = require("../db/models/userImages")
+const { uploadToS3 } = require("../helpers")
+const { addImageContextData, getImageData } = require("../db/models/userImages")
 const uploadFile = multer()
 const OpenAI = require("openai")
 
-const openAI = async(imageUrl)=>{
+const openAI = async (imageUrl) => {
     const openai = new OpenAI({
         apiKey: process.env.OPEN_AI,
     });
@@ -19,7 +19,7 @@ const openAI = async(imageUrl)=>{
        4. Stone count (if visible, else "Unknown")
        Return valid JSON: { category, title, description, stone_count }
      `
-     const aiRes = await openai.chat.completions.create({
+    const aiRes = await openai.chat.completions.create({
         model: "gpt-4o-mini",
         messages: [
             {
@@ -28,7 +28,7 @@ const openAI = async(imageUrl)=>{
                     { type: "text", text: prompt },
                     {
                         type: "image_url",
-                        image_url: {url: imageUrl.url, detail:"low"}
+                        image_url: { url: imageUrl.url, detail: "low" }
                     }
                 ]
             }
@@ -42,27 +42,37 @@ const openAI = async(imageUrl)=>{
 router.post("/upload", uploadFile.single("image"), async (req, res) => {
     console.log("---------------", req.file)
 
-    if(!req.file){
-        return res.json({result:false, value:"Image is required"})
+    if (!req.file) {
+        return res.json({ result: false, value: "Image is required" })
     }
     console.log("---------------", req.body)
     const imageUrl = await uploadToS3(req.file)
 
     const aiContent = await openAI(imageUrl)
-      console.log("aiconte", aiContent)
+    console.log("aiconte", aiContent)
     let object = {
         category: aiContent.category,
         title: aiContent.title,
         description: aiContent.description,
-        stone_count: aiContent.stone_count === "Unknown"?0:aiContent.stone_count,
-        url: imageUrl?.url,
+        stone_count: aiContent.stone_count === "Unknown" ? 0 : aiContent.stone_count,
+        signedUrl: imageUrl?.url,
     }
     console.log(object);
-    
+
     const addData = await addImageContextData(object)
     console.log("******************", addData)
-      
+
     return res.json(addData)
 })
 
-module.exports= router
+router.get("/images", async (req, res) => {
+    try {
+        let result = await getImageData()
+        res.json(result)
+    } catch (err) {
+        res.json({ resutl: false, value: err })
+    }
+
+})
+
+module.exports = router
